49b904e61b598ba82bb3eb059934160a27c62840
rubencosta13 <rubencosta13@users.noreply.github.com>
2021-01-10 21:47:05 +0000

    Initial commit

---
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.rolldice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+    }
+}
+
+
+
+
+
+
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+private fun rollDice() {
+    val dice = Dice(6)
+    val diceRoll = dice.roll()
+    val resultTextView: TextView = findViewById(R.id.textView)
+    resultTextView.text = diceRoll.toString()
+
+}
+
+
+
